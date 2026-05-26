54057c99e34a3fc1b48bf2a2794f0d7901162327
Fun1984 <sorkejso@gmail.com>
2026-05-26 13:31:11 +0900

    Backup

--- src/components/CreateImageForm.jsx
+++ src/components/CreateImageForm.jsx
@@ -6,7 +6,7 @@ function CreateImageForm({title, author, content}) {
     const [createdAt, setCreatedAt] = useState('');
     const [updatedAt, setUpdatedAt] = useState('');
     const [quality, setQuality] = useState('middle');
-    const [ai_api_key, setAi_api_key] = useState('');
+    const [ai_api_key, setAi_api_key] = useState('정책에 의해 프론트를 통한 제공 안됨');
     const [coverImageUrl, setCoverImageUrl] = useState('./test_src/01.png');
     
     const [loading, setLoading] = useState(false);
@@ -18,131 +18,10 @@ function CreateImageForm({title, author, content}) {
         setCreatedAt(day);
         setUpdatedAt(day);
     }, []);
-    function resizeBase64Image(base64, maxWidth = 512) {
-    return new Promise((resolve) => {
-        const img = new Image();
-
-        img.src = base64;
-
-        img.onload = () => {
-            const canvas = document.createElement('canvas');
-            const ctx = canvas.getContext('2d');
-
-            const scale = maxWidth / img.width;
-
-            canvas.width = maxWidth;
-            canvas.height = img.height * scale;
-
-            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-            // PNG 유지
-            const resizedBase64 = canvas.toDataURL('image/png');
-
-            resolve(resizedBase64);
-            };
-        });
-    }
-    // 책 정보 db 저장을 선행할 경우, 사용. 
-    // 지금 구조에서는 AI 생성까지 다 하고 저장하기로 함.
-    // const handleAddBook = async (newBook) => {
-    // try {
-    //   const res = await fetch('http://localhost:3000/books', {
-    //     method: 'POST',
-    //     headers: {'Content-Type': 'application/json'},
-    //     body: JSON.stringify(newBook)
-    //   });
-    // //   console.log(res.ok)
-    //    const text = await res.text();
-    // //    console.log(text);
-    //   const saved = await res.json();
-    //   setPosts([saved, ...posts]);
-    // } catch (err) { console.error(err); }
-    // };
-    
-    // const handleAIImage = async () => {
-    //     const prompt = `
-    //                     # 역할
-    //                     너는 북커버 제작 담당자야. 
-                        
-    //                     # 지침
-    //                     - 북커버의 앞면 표지만을 보여줄 것
-    //                     - 전문적인 북커버 디자인, 높은 퀄리티의 일러스트레이션, 두드러진 시각적 표현, 작품에 적합한 안전성
-    //                     - 이야기의 분위기나 무드를 포함
-                        
-    //                     # 책 정보
-    //                     - 제목 : "${title}"
-    //                     - 내용 요약 : ${content}.
-    //                     `
-    //     // 1. AI Image 생성
-    //     try {
-    //         if (loading === false) {
-    //             setCoverImageUrl('./test_src/loading.gif');
-    //             setLoading(true);
-    //         }
-            
-    //         const res = await fetch('https://api.openai.com/v1/images/generations', {
-    //         method: 'POST',
-    //         headers: {'Content-Type': 'application/json',
-    //                   'Authorization': `Bearer ${API_KEY}`
-    //                  },
-    //         body: JSON.stringify({
-    //                                 model: 'gpt-image-1',
-    //                                 prompt,
-    //                                 n: 1,
-    //                                 size: '1024x1536',
-    //                                 quality,
-    //                                 output_format: 'png'
-    //                             }),
-    //                 });
-    //         setLoading(false);
-
-    //         if (!res.ok) {
-    //             setCoverImageUrl('./test_src/error.png');
-    //             const errData = await res.json().catch(() => ({}))
-    //             const status = res.status
-    //             if (status === 401) throw new Error('API Key가 올바르지 않습니다. 확인 후 다시 시도해주세요.')
-    //             if (status === 429) throw new Error('요청 한도를 초과했습니다. 잠시 후 다시 시도해주세요.')
-    //             throw new Error(errData?.error?.message || 'OpenAI 이미지 생성에 실패했습니다.')
-    //         }
-
-    //         const data = await res.json();
-    //         const b64Json = data?.data?.[0]?.b64_json;
-    //         if (!b64Json) throw new Error('이미지 데이터를 받지 못했습니다.');
-            
-    //         const imageUrl = `data:image/png;base64,${b64Json}`;
-    //         const resizedImage = await resizeBase64Image(imageUrl, 512);
-    //         setCoverImageUrl(resizedImage);
-    //         } catch (err) { console.error(err); }
-    // }
-
-    
-    // function resizeBase64Image(base64, maxWidth = 512) {
-    // return new Promise((resolve) => {
-    //     const img = new Image();
-
-    //     img.src = base64;
-
-    //     img.onload = () => {
-    //         const canvas = document.createElement('canvas');
-    //         const ctx = canvas.getContext('2d');
-
-    //         const scale = maxWidth / img.width;
-
-    //         canvas.width = maxWidth;
-    //         canvas.height = img.height * scale;
-
-    //         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
-    //         // PNG 유지
-    //         const resizedBase64 = canvas.toDataURL('image/png');
-    //         setCoverImageUrl(resizedBase64)
-    //         resolve(resizedBase64);
-    //         };
-    //     });
-    // }
 
     const handleFinalForm = async () => {
-            const prompt = `
+        let imageUrl;
+        const prompt = `
                         # 역할
                         너는 북커버 제작 담당자야. 
                         
@@ -162,20 +41,22 @@ function CreateImageForm({title, author, content}) {
                 setLoading(true);
             }
             
-            const res = await fetch('https://api.openai.com/v1/images/generations', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json',
-                      'Authorization': `Bearer ${ai_api_key}`
-                     },
-            body: JSON.stringify({
-                                    model: 'gpt-image-1',
-                                    prompt,
-                                    n: 1,
-                                    size: '1024x1536',
-                                    quality,
-                                    output_format: 'png'
-                                }),
-                    });
+            const res = await fetch("http://localhost:3001/api/image", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    model: "gpt-image-2",
+                    prompt,
+                    n : 1,
+                    size: "1536x1024",
+                    quality,
+                    output_format: 'jpeg',
+                    output_compression: 50
+                }),
+            });
+            console.log('1');
             setLoading(false);
 
             if (!res.ok) {
@@ -188,20 +69,27 @@ function CreateImageForm({title, author, content}) {
             }
 
             const data = await res.json();
+
+            console.log("OPENAI RESPONSE:", data);
             const b64Json = data?.data?.[0]?.b64_json;
+            
             if (!b64Json) throw new Error('이미지 데이터를 받지 못했습니다.');
             
-            const imageUrl = `data:image/png;base64,${b64Json}`;
-            // const resizedImage = await resizeBase64Image(imageUrl, 512);
+            imageUrl = `data:image/jpeg;base64,${b64Json}`;
+
             setCoverImageUrl(imageUrl);
+            
             } catch (err) { console.error(err); }
             // 2. db 저장
-            // resizeBase64Image(coverImageUrl)
-            
-            const newBook = { title, content, author,
-                              likes:0, views:0, imageUrl: coverImageUrl,
-                              createdAt, updatedAt }
             try {
+                const newBook = { title, 
+                              content, 
+                              author,
+                              likes:0, 
+                              views:0, 
+                              imageUrl,
+                              createdAt, 
+                              updatedAt }
                 const res = await fetch('http://localhost:3000/books', {
                     method: 'POST',
                     headers: {'Content-Type': 'application/json'},
@@ -218,7 +106,7 @@ function CreateImageForm({title, author, content}) {
             <input value={ai_api_key} onChange={(e) => setAi_api_key(e.target.value)}></input>
             <p>품질 :</p> <Dropdown value={quality} onChange={setQuality} />
         </div>
-        <div><img src={coverImageUrl} alt="book cover" style={{ width: '200px', height: 'auto'}}></img></div>
+        <div><img src={coverImageUrl} alt="book cover" style={{ width: '400px', height: 'auto'}}></img></div>
         <div>
             <h3>책 내용</h3>
             <p>{content}</p>
